3b03dc0803f1df06a769f7844ebb595c4829cd95
kwak <sal091625@gmail.com>
2024-11-08 16:11:02 +0900

    feat: makeRoom 유효성 검사, timeline 비회원 toast 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import mainRouter from '../src/routers/main-router';
 import { RouterProvider } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ToastContainer } from 'react-toastify';
 
 import { AuthProvider } from './components/contexts/AuthContext';
 function App() {
   return (
     <AuthProvider>
+      <ToastContainer />
       <RouterProvider router={mainRouter} />
     </AuthProvider>
   );
