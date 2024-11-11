f6bb2521c75104d8763e3409fd7382a316bf5b2d
kwak <sal091625@gmail.com>
2024-11-11 13:04:47 +0900

    feat: 파도 애니메이션 배경화면 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'wave-move': 'wave-move 12s linear infinite',
+      },
+      keyframes: {
+        'wave-move': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 };
